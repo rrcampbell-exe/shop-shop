3ea6d178d02094b173db26a3ad6cd119f00329c1
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-10 11:03:24 -0500

    Implement shopping cart, functionality for same

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,9 +1,15 @@
-import { useReducer } from 'react';
+import { useReducer } from "react";
 
 import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART,
 } from "./actions";
 
 export const reducer = (state, action) => {
@@ -25,6 +31,61 @@ export const reducer = (state, action) => {
       return {
         ...state,
         currentCategory: action.currentCategory,
+      };
+
+    // if action type value is `ADD_TO_CART`, return a new state object with updated cart
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+
+    // if action type value is `ADD_MULTIPLE_TO_CART`, return a new state object with all items in updated cart
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+
+    // if actiont ype value is `REMOVE_FROM_CART`, return a new state object that adjusts the cartOpen property based on length of newState
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+
+    // if action value type is `UPDATE_CART_QUANTITY`, return new state object that updates quantity of only items whose ids match those selected by customer
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+
+    // if action value type is `CLEAR_CART`, empty all items from cart and set cartOpen to false
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+
+    // if action value type is `TOGGLE_CART`, set cartOpen to opposite boolean value of current state
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
       };
 
     // if it's none of these actions, do not update state at all and keep things the same!
