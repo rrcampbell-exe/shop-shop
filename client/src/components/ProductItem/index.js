e99127b9f7508c56ff0cb53d582875f1f024aad0
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-12 05:45:08 -0500

    Implement IndexedDB for offline data persistence

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { pluralize } from "../../utils/helpers";
+import { pluralize, idbPromise } from "../../utils/helpers";
 import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 
@@ -9,27 +9,28 @@ function ProductItem(item) {
 
   const [state, dispatch] = useStoreContext();
 
-  const { cart } = state
+  const { cart } = state;
 
   const addToCart = () => {
-    // find the cart item with the matching id
     const itemInCart = cart.find((cartItem) => cartItem._id === _id);
-  
-    // if there was a match, call UPDATE with a new purchase quantity
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: _id,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
+      idbPromise("cart", "put", {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
       });
     } else {
       dispatch({
         type: ADD_TO_CART,
-        product: { ...item, purchaseQuantity: 1 }
+        product: { ...item, purchaseQuantity: 1 },
       });
+      idbPromise("cart", "put", { ...item, purchaseQuantity: 1 });
     }
   };
-  
 
   return (
     <div className="card px-1 py-1">
